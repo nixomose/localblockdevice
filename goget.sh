ae69efc1219e9a365897636cb84e1c016306d000
nixomose <nixomose@gmail.com>
2022-06-05 07:23:08 -0400

    goget script

---
+++ goget.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd ..
+git clone nixo@192.168.123.210:ford/git/localblockdevice
+git clone nixo@192.168.123.210:ford/git/compressblockdevice
+git clone nixo@192.168.123.210:ford/git/nixomosegotools
+git clone nixo@192.168.123.210:ford/git/zosbd2goclient
+git clone nixo@192.168.123.210:ford/git/stree_v
+git clone nixo@192.168.123.210:ford/git/slookup_i
+git clone nixo@192.168.123.210:ford/git/blockdevicelib
+
+
